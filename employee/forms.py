5341648273539c3e9d87a88727294290eab0d82b
talhajubair100 <talhajubair100.bd@gmail.com>
2020-12-12 16:17:22 +0600

    bug fix

--- employee/forms.py
+++ employee/forms.py
@@ -18,8 +18,8 @@ class EmployeCreationForm(forms.ModelForm):
                 self.fields['district'].queryset = District.objects.filter(division_id=division_id).order_by('name')
             except (ValueError, TypeError):
                 pass  # invalid input from the client; ignore and fallback to empty City queryset
-        elif self.instance.pk:
-            self.fields['district'].queryset = self.instance.division.district_set.order_by('name')
+        # elif self.instance.pk:
+        #     self.fields['district'].queryset = self.instance.division.district_set.order_by('name')
 
 # contry = Division
 # city = District            
--- employee/views.py
+++ employee/views.py
@@ -9,7 +9,7 @@ from address .models import District, Division
 # Create your views here.
 @login_required
 def employe_create_view(request):
-    employe_profile = EmployeUser.objects.get(emp_user=request.user)
+    employe_profile = EmployeUser.objects.get_or_create(emp_user=request.user)
     form = EmployeCreationForm(instance=employe_profile)
     if request.method == 'POST':
         form = EmployeCreationForm(request.POST, request.FILES, instance=employe_profile)
